043265791d0e4cd097b4636c2cddc5f89a184958
Guide Fari <guideg6@gmail.com>
2024-05-20 08:13:22 +0200

    iac: define dynamo table

--- go_cdk.go
+++ go_cdk.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awsdynamodb"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
 	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
 	"github.com/aws/constructs-go/constructs/v10"
@@ -19,11 +20,22 @@ func NewGoCdkStack(scope constructs.Construct, id string, props *GoCdkStackProps
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
 
-	awslambda.NewFunction(stack, jsii.String("goCDKLambdaFunction"), &awslambda.FunctionProps{
+	table := awsdynamodb.NewTable(stack, jsii.String("myUserTable"), &awsdynamodb.TableProps{
+		PartitionKey: &awsdynamodb.Attribute{
+			Name: jsii.String("username"),
+			Type: awsdynamodb.AttributeType_STRING,
+		},
+		// FIXME: typically good practice to pull this in from env var
+		TableName: jsii.String("userTable"),
+	})
+
+	myFunction := awslambda.NewFunction(stack, jsii.String("goCDKLambdaFunction"), &awslambda.FunctionProps{
 		Runtime: awslambda.Runtime_PROVIDED_AL2023(),
 		Code:    awslambda.AssetCode_FromAsset(jsii.String("lambda/function.zip"), nil),
 		Handler: jsii.String("main"),
 	})
+
+	table.GrantReadWriteData(myFunction)
 
 	// queue := awssqs.NewQueue(stack, jsii.String("GoCdkQueue"), &awssqs.QueueProps{
 	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
